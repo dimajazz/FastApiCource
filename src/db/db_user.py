a45bf1de0a29a2fc23ddfd8e113f1b23f3719010
dimajazz <dimazuh@gmail.com>
2023-11-07 19:30:44 +0200

    created: db definition, model, schema, orm functionality, hashing password

---
+++ src/db/db_user.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm.session import Session
+
+from db.schemas import UserBase
+from db.models import DBUser
+from db.hash import Hash
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = DBUser(
+        username=request.username,
+        email=request.email,
+        password=Hash(request.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+
 from routes import blog_get_routes
 from routes import blog_post_routes
+
+from db import models
+from db.database import engine
+
 
 app = FastAPI()
 app.include_router(blog_get_routes.router)
@@ -13,3 +18,6 @@ app.include_router(blog_post_routes.router)
          response_description='Return a Hello world message')
 def index():
     return {'message': 'Hello world!'}
+
+
+models.Base.metadata.create_all(engine)
